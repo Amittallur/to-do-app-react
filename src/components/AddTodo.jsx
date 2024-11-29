ea07d27c31103a4fb26404db3ad403e84e83af7d
Amit Tallur <amittallur@Amits-MacBook-Air.local>
2024-11-29 10:54:33 +0530

    add and delete todos

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,34 +2,73 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTodo } from '../redux/todoSlice';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link } from "@nextui-org/react";
 
 const AddTodo = () => {
-  const [todoText, setTodoText] = useState('');
-  const dispatch = useDispatch();
+    const [todoText, setTodoText] = useState('');
 
-  const handleAddTodo = () => {
-    if (todoText.trim()) {
-      const newTodo = {
-        id: Date.now(),
-        text: todoText,
-        completed: false,
-      };
-      dispatch(addTodo(newTodo));
-      setTodoText('');
-    }
-  };
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const dispatch = useDispatch();
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={todoText}
-        onChange={(e) => setTodoText(e.target.value)}
-        placeholder="Add a new to-do"
-      />
-      <button onClick={handleAddTodo}>Add Todo</button>
-    </div>
-  );
+    const handleAddTodo = () => {
+        onOpenChange();
+        if (todoText.trim()) {
+            const newTodo = {
+                id: Date.now(),
+                text: todoText,
+                completed: false,
+            };
+            dispatch(addTodo(newTodo));
+            setTodoText('');
+        }
+    };
+
+    return (
+        <div>
+            <div className='flex justify-center items-center'>
+                <h2 className='text-2xl font-bold text-white'>To-Do App</h2>
+            </div>
+
+
+
+
+            <div className='flex justify-center items-center p-4'>
+
+                <Button onPress={onOpen} color='primary'>Add Todo</Button>
+                <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                    <ModalContent>
+                        {(onClose) => (
+                            <>
+                                <ModalHeader className="flex flex-col gap-1">Todos</ModalHeader>
+                                <ModalBody>
+                                    <div className='w-full'>
+                                        <Input
+                                            type="text"
+                                            value={todoText}
+                                            onChange={(e) => setTodoText(e.target.value)}
+                                            placeholder="Add a new to-do"
+                                        />
+                                       
+                                    </div>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" variant="light" onPress={onClose}>
+                                        Close
+                                    </Button>
+                                    <Button color="primary"  onClick={handleAddTodo} >
+                                        Add
+                                    </Button>
+                                </ModalFooter>
+                            </>
+                        )}
+                    </ModalContent>
+                </Modal>
+            </div>
+
+
+
+        </div>
+    );
 };
 
 export default AddTodo;
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTodo, removeTodo } from '../redux/todoSlice';
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell,Button} from "@nextui-org/react";
+
 
 const ToDoList = () => {
   const todos = useSelector((state) => state.todos.todos);
@@ -15,23 +17,37 @@ const ToDoList = () => {
     dispatch(removeTodo(id));
   };
 
-  return (
-    <div>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span
-              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-              onClick={() => handleToggleTodo(todo.id)}
-            >
-              {todo.text}
-            </span>
-            <button onClick={() => handleRemoveTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+return (
+    <div className='flex justify-center items-center p-4'>
+        <div className='flex justify-center items-center w-1/3'>
+        {todos.length>0 && (
+            <Table aria-label="To-Do List">
+                <TableHeader>
+                    <TableColumn>Task</TableColumn>
+                    <TableColumn>Actions</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {todos.map((todo) => (
+                        <TableRow key={todo.id}>
+                            <TableCell>
+                                <span
+                                    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+                                    onClick={() => handleToggleTodo(todo.id)}
+                                >
+                                    {todo.text}
+                                </span>
+                            </TableCell>
+                            <TableCell>
+                                <Button onClick={() => handleRemoveTodo(todo.id)} size='sm'>Delete</Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        )}
     </div>
-  );
+        </div>
+);
 };
 
 export default ToDoList;
